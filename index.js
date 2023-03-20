9326d8b0424f89a188b2c22c35d4292962dead88
saradonnelly97 <sara.donnelly8@gmail.com>
2023-03-20 17:09:24 -0400

    Added the play again loop

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 // Record User's Name
-let userName = window.prompt('Please enter your name.')
-let userScore = 0
+let userName = window.prompt('Please enter your name.');
+let userScore = 0;
+let playAgain = true;
 
-for (let i = 0; i < questions.length; i++) {
-    console.log(questions[i])
-    let question = questions[i]
-    let userAnswer = window.prompt(question.text) 
-    if(userAnswer === question.correctAnswer) {
-        userScore = userScore + 10
+function playGame() {
+    for (let i = 0; i <questions.length; i++) {
+        let question = questions[i]
+        let userAnswer = window.prompt(question.text)
+        if (userAnswer === question.correctAnswer) {
+            userScore = userScore + 10
+        }
     }
+    window.alert('Your score is: ' + userScore)
 }
 
-window.alert ('Your score is:' + userScore)
+ 
+
+while (playAgain === true) {
+    playGame()
+    let userChoice = window.prompt('Would you like to play the game again? Answer yes or no.')
+    if(userChoice === 'yes'){
+            playAgain = true
+        } else {
+            playAgain = false
+            window.alert('Thanks for playing the game!')
+        }
+}
+
