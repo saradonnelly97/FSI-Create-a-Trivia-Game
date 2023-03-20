51bb959f6fc01fb90307d8e97f5140cf24cc1022
saradonnelly97 <sara.donnelly8@gmail.com>
2023-03-20 16:59:07 -0400

    Added the initial game code

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your Code Here
+// Record User's Name
+let userName = window.prompt('Please enter your name.')
+let userScore = 0
+
+for (let i = 0; i < questions.length; i++) {
+    console.log(questions[i])
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text) 
+    if(userAnswer === question.correctAnswer) {
+        userScore = userScore + 10
+    }
+}
+
+window.alert ('Your score is:' + userScore)
